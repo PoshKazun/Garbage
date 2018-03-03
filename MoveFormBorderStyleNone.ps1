0706eee9b95126b4e52c595e02af37e90cc391a8
PoshKazun <31365593+PoshKazun@users.noreply.github.com>
2018-03-03 11:18:06 +0300

    Add files via upload

---
+++ MoveFormBorderStyleNone.ps1
@@ -0,0 +1,16 @@
+﻿#Requires -PSEdition Desktop
+
+using assembly System.Windows.Forms
+using namespace System.Windows.Forms
+
+$form = [Form]@{
+	FormBorderStyle = "None"
+}
+
+$form.Add_MouseDown({
+	$form.Capture = $false
+	$m = [Message]::Create($form.Handle, 0xa1, (New-Object IntPtr(2)), [IntPtr]::Zero)
+	$form.WindowTarget.DefWndProc([ref]$m)
+})
+
+$form.ShowDialog()
